ed9f5a51702c6de86e2f2a630dd75117124f0a8c
masaki <ikeda.masaki@gmail.com>
2019-09-27 14:43:16 +0900

    remove check option

--- local_provision.sh
+++ local_provision.sh
@@ -7,4 +7,4 @@ apt-get update -y
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y ansible
 
-ansible-playbook -i provision/inventories/develop/hosts provision/site.yml --check
+ansible-playbook -i provision/inventories/develop/hosts provision/site.yml
